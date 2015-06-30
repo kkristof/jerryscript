c8e8a2d21152c70d5fa1ffa4b8f0c89bdc1d327a
Ruben Ayrapetyan <r.ayrapetyan@samsung.com>
2015-06-30 21:53:41 +0300

    Add several interfaces for checking if character belongs to a ECMA-defined character class, without implementation.
    Interfaces are added for the following classes:
     - format control characters:
      - zero width non-joiner;
      - zero width joiner;
      - byte order mark;
     - white space characters:
      - tab;
      - vertical tab;
      - form feed;
      - space;
      - no-break space;
      - unicode space separator;
     - line terminator characters:
      - line separator;
      - paragraph separator;
     - identifier names characters:
      - dollar sign;
      - lower line;
      - backslash;
      - unicode combining mark;
      - unicode digit;
      - unicode connector punctuation.
    
    JerryScript-DCO-1.0-Signed-off-by: Ruben Ayrapetyan r.ayrapetyan@samsung.com

--- jerry-core/lit/lit-char-helpers.h
+++ jerry-core/lit/lit-char-helpers.h
@@ -18,11 +18,50 @@
 
 #include "lit-globals.h"
 
-extern bool lit_char_is_unicode_letter (ecma_char_t);
+/*
+ * Format control characters (ECMA-262 v5, Table 1)
+ */
+extern bool lit_char_is_zero_width_non_joiner (ecma_char_t);
+extern bool lit_char_is_zero_width_joiner (ecma_char_t);
+extern bool lit_char_is_byte_order_mark (ecma_char_t);
+extern bool lit_char_is_format_control (ecma_char_t);
+
+/*
+ * Whitespace characters (ECMA-262 v5, Table 2)
+ */
+extern bool lit_char_is_tab (ecma_char_t);
+extern bool lit_char_is_vertical_tab (ecma_char_t);
+extern bool lit_char_is_form_feed (ecma_char_t);
+extern bool lit_char_is_space (ecma_char_t);
+extern bool lit_char_is_no_break_space (ecma_char_t);
+extern bool lit_char_is_space_separator (ecma_char_t);
+extern bool lit_char_is_white_space (ecma_char_t);
+
+/*
+ * Line terminator characters (ECMA-262 v5, Table 3)
+ */
 extern bool lit_char_is_new_line (ecma_char_t);
 extern bool lit_char_is_carriage_return (ecma_char_t);
+extern bool lit_char_is_line_separator (ecma_char_t);
+extern bool lit_char_is_paragraph_separator (ecma_char_t);
 extern bool lit_char_is_line_terminator (ecma_char_t);
+
+/*
+ * Identifier name characters (ECMA-262 v5, 7.6)
+ */
+extern bool lit_char_is_dollar_sign (ecma_char_t);
+extern bool lit_char_is_low_line (ecma_char_t);
+extern bool lit_char_is_backslash (ecma_char_t);
+extern bool lit_char_is_unicode_letter (ecma_char_t);
+extern bool lit_char_is_unicode_combining_mark (ecma_char_t);
+extern bool lit_char_is_unicode_digit (ecma_char_t);
+extern bool lit_char_is_unicode_connector_punctuation (ecma_char_t);
+
+/*
+ * Part of IsWordChar abstract operation (ECMA-262 v5, 15.10.2.6, step 3)
+ */
 extern bool lit_char_is_word_char (ecma_char_t);
+
 extern bool lit_char_hex_to_int (ecma_char_t);
 
 #endif /* LIT_CHAR_HELPERS_H */
